dfc55bbb25e2a93ff46eddb978b3472ec85463ca
Ahn <nha2942004@gmail.com>
2023-02-28 20:21:20 +0700

    Tinh chu vi, dien tich hinh tron

---
+++ bt2.py
@@ -0,0 +1,7 @@
+#Tính chu vi,diện tích hình tròn
+
+import math
+
+r = float(input("Nhap ban kinh hinh tron: "))
+print("dien tich hinh tron: ", math.pi*r*r)
+print("chu vi hinh tron: ", 2 * math.pi * r)
